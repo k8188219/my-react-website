1a2528c59f07771477f85df414f0ae23ca818b1c
kevin <k8188219@gmail.com>
2020-06-02 15:12:54 +0800

    ver-0.1.1-build add spinner and tooltiip change to static

---
+++ precache-manifest.50ea1ed5e331c29150757e166d7ae255.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a661388ca0c9e85dafe1407d924ee96",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a6b8b3bf3495fec5d164",
+    "url": "./static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "7a6aefadedca49347fc4",
+    "url": "./static/js/2.1437d13e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1437d13e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b8b3bf3495fec5d164",
+    "url": "./static/js/main.f7144d62.chunk.js"
+  },
+  {
+    "revision": "a69bc061cf7a129a4160",
+    "url": "./static/js/runtime-main.86a8c492.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2fbca18627a2336a32ea56c8f51933dc.js"
+  "./precache-manifest.50ea1ed5e331c29150757e166d7ae255.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
